a0122983917090593fd21fd8f09011febde3bfdf
CodeWithRitik01 <rkritik.9958@gmail.com>
2023-09-25 20:11:32 +0530

    made chnges

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,3 @@
 module.exports.profile = function(req, res){
-   return res.end('<h1>Users profile</h1>');
-}
+    res.end('<h1>Users profile</h1>');
+} 
--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 8000;
 
 // use express router
 app.get('/', require('./routes'));
+
+// set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in the running the server : ${err}`);
