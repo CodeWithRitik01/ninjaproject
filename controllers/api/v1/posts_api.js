35b0646daa3de23b9af71644d711a93d4a6eb601
CodeWithRitik01 <rkritik.9958@gmail.com>
2023-10-25 19:14:13 +0530

    created api

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,51 @@
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comments');
+
+module.exports.index = async function(req, res){
+    let posts = await Post.find({})
+    .sort('-createdAt')
+    .populate('user').populate({
+      path: 'comments',
+      populate: {
+         path: 'user'
+      }
+    });
+
+    return res.json(200, {
+        message: "List of posts",
+        post: posts
+    })
+}
+
+
+
+
+module.exports.destroy = async function(req, res){
+    try{
+        const post = await Post.findById(req.params.id);
+        //.id means consverting the object id into string
+        if(post.user == req.user.id){
+            await post.removePost();
+            await Comment.deleteMany({post: req.params.id}).exec();
+
+            
+
+
+           return res.json(200, {
+            message: "Post and associated comments are deleted successfully!"
+           });
+        }else{
+          return res.json(200, {
+            message: "You cannot delete this post!"
+          });
+        }
+    }catch(err){
+        console.log("$$$$$$", err);
+
+        return res.json(500, {
+            
+            message: "Internal Server Error"
+        });
+    }
+}
+
